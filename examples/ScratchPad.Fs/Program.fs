fa4678d3ed4bfb32e208f9abfa00a33dcaa2b34b
Martijn Laarman <Mpdreamz@gmail.com>
2024-01-05 22:41:58 +0100

    feature/fsharp bindings (#7)
    * Introduce F# CE bindings through Proc.Fs
    
    * update build scripts
    
    * update build scripts
    
    * update build scripts
    
    * update build scripts
    
    * update tests
    
    * update waittime
    
    * update waittime
    
    * update build tools
    
    * mark ScratchPad.Fs as unpackable
    
    * ensure we check for build info in InformationalVersion
    
    * update minver reference

---
+++ examples/ScratchPad.Fs/Program.fs
@@ -0,0 +1,19 @@
+﻿open Proc.Fs
+
+let uname = shell {
+    exec "dotnet" "--version"
+    exec "uname" 
+}
+
+let dotnetVersion = exec {
+    binary "dotnet"
+    args "--help"
+    filter (fun l -> l.Line.Contains "clean")
+}
+
+printfn "Found lines %i" dotnetVersion.Length
+
+let dotnetRestoreHelp = exec {
+    binary "dotnet"
+    invoke_args ["restore"; "--help"]
+}
