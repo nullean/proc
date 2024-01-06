4a906e72a431988ad105cba4cb1f1a58e896ce63
Martijn Laarman <Mpdreamz@gmail.com>
2024-01-06 22:50:03 +0100

    improve F# bindings usage (#9)
    * improve F# bindings usage
    
    * Quick README's
    
    * Quick README's
    
    * ensure readme gets packaged

--- examples/ScratchPad.Fs/Program.fs
+++ examples/ScratchPad.Fs/Program.fs
@@ -1,19 +1,48 @@
-﻿open Proc.Fs
+﻿open System
+open Proc.Fs
 
-let uname = shell {
+let _ = shell {
     exec "dotnet" "--version"
-    exec "uname" 
+    exec "uname"
 }
 
 let dotnetVersion = exec {
     binary "dotnet"
     args "--help"
+    filter_output (fun l -> l.Line.Contains "clean")
     filter (fun l -> l.Line.Contains "clean")
+}
+
+exec {
+    binary "dotnet"
+    args "--help"
+    env Map[("key", "value")]
+    workingDirectory "."
+    send_control_c false
+    timeout (TimeSpan.FromSeconds(10))
+    thread_wrap false
+    validExitCode (fun i -> i <> 0)
+    run
+}
+
+let helpStatus = exec {
+    binary "dotnet"
+    args "--help"
+    exit_code
+}
+
+let helpOutput = exec {
+    binary "dotnet"
+    args "--help"
+    output
 }
 
 printfn "Found lines %i" dotnetVersion.Length
 
-let dotnetRestoreHelp = exec {
+exec {
     binary "dotnet"
-    invoke_args ["restore"; "--help"]
+    run_args ["restore"; "--help"]
 }
+
+exec { run "dotnet" " "}
+let statusCode = exec { exit_code_of "dotnet" " "}
